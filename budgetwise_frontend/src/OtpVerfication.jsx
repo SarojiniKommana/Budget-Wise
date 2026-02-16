7cc8033052d87f175da756f0f855b7a81a28527f
SarojiniKommana <sarojinikommana916@gmail.com>
2026-02-16 18:54:13 +0530

    Update OtpVerfication.jsx

--- budgetwise_frontend/src/OtpVerfication.jsx
+++ budgetwise_frontend/src/OtpVerfication.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import "./Dashboard.css";
 
 function OtpVerification() {
   const navigate = useNavigate();
